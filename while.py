7ae8729ed75828c32389572bbe8bbad3ef5185fe
ism47325595 <ism47325595@a10.informatica.escoladeltreball.org>
2018-01-29 12:50:44 +0100

    first commit

---
+++ while.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+num = int("Inserte numero entero:")
+num2 = int("Inserte otro numero entero mayor que el primero:")
+
+
+while (num > num2):
+	num2=input("Intentalo de nuevo:")		
+		print "Los numeros que has escrito son", num, "y", num2
+

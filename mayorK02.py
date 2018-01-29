9cd0390ebbdbc857a21088255fa0c735d166b89e
Clàudia Martín <32127295+ClaudiaMartin00@users.noreply.github.com>
2018-01-29 13:23:55 +0100

    Create mayorK02.py

---
+++ mayorK02.py
@@ -0,0 +1,8 @@
+#coding: utf-8
+
+num = float(input("Escriba un numero:")
+num2 = float(input("Escriba un numero mayor al primero:")
+
+while (num<num2):
+	num=float("Intentalo de nuevo")	
+		print num, "No es mayor que", num2
